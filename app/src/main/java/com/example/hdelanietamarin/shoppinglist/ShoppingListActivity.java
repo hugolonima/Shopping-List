c337d92d6508a17945ff107c50e2d3099164f443
hugo <hugolonima@gmail.com>
2017-10-19 21:12:27 +0200

    Botón remove hecho

--- app/src/main/java/com/example/hdelanietamarin/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/hdelanietamarin/shoppinglist/ShoppingListActivity.java
@@ -1,9 +1,12 @@
 package com.example.hdelanietamarin.shoppinglist;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -48,8 +51,33 @@ public class ShoppingListActivity extends AppCompatActivity {
                 return true;
             }
         });
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> list, View item, int pos, long id) {
+                maybeRemoveItem(pos);
+                return true;
+            }
+        });
 
         list.setAdapter(adapter);
+    }
+
+    private void maybeRemoveItem(final int pos) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.confirm);
+        builder.setMessage(String.format(
+                "Are you sure you want to remove '%s'?",
+                itemList.get(pos)));
+        builder.setPositiveButton(R.string.remove, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        itemList.remove(pos);
+                        adapter.notifyDataSetChanged();
+                    }
+                });
+        builder.setNegativeButton(android.R.string.cancel, null);
+        builder.create().show();
+
     }
 
     private void addItem() {
