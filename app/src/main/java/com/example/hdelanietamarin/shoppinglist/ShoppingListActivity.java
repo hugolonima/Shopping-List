24ea32498eb3e2d8881e9b6808618e4976cc3a94
hugo <hugolonima@gmail.com>
2017-10-19 20:18:54 +0200

    Lista hecha

--- app/src/main/java/com/example/hdelanietamarin/shoppinglist/ShoppingListActivity.java
+++ app/src/main/java/com/example/hdelanietamarin/shoppinglist/ShoppingListActivity.java
@@ -2,12 +2,16 @@ package com.example.hdelanietamarin.shoppinglist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-public class ShoppingListActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class ShoppingListActivity extends AppCompatActivity {
+    private ArrayList<String> itemList;
+    private ArrayAdapter<String> adapter;
     private ListView list;
     private Button btn_add;
     private EditText edit_item;
@@ -19,5 +23,15 @@ public class ShoppingListActivity extends AppCompatActivity {
         list = (ListView) findViewById(R.id.list);
         btn_add = (Button) findViewById(R.id.btn_add);
         edit_item = (EditText) findViewById(R.id.edit_item);
+        itemList = new ArrayList<>();
+        itemList.add("Patatas");
+        itemList.add("Papel de vater");
+        itemList.add("Zanahorias");
+        itemList.add("Yogures");
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                itemList);
+
+        list.setAdapter(adapter);
     }
 }
